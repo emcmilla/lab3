cb3b19c8c9f72ca9ddf0a505fd30f89fcffd78b0
Erich McMillan <emcmilla@ucsd.edu>
2019-01-24 10:15:06 -0800

    Done

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,10 +9,31 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$(".jumbotron p").addClass("active");
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$("a.thumbnail").click(projectClick);
+		$('.jumbotron h1').text("Javascript Loaded");
+		//$('.jumbotron #testjs').text("All good");
+		$("#testjs").text("Please Wait");
+		$(".jumbotron p").toggleClass("active");
 	});
 
+	function projectClick(e) { 
+			console.log("Project Clicked");
+	    // prevent the page from reloading 
+	    e.preventDefault();
+	    // In an event handler, $(this) refers to 
+	    // the object that triggered the event 
+	    $(this).css("background-color", "#7fff00");
+
+			var containingProject = $(this).closest(".project");
+    	var description = $(containingProject).find(".project-description");
+    	if (description.length == 0) {
+       	$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    	} else {
+       	description.fadeToggle();
+    	}
+	}
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
